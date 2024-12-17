1f059a584aea4a08c55eda250f314d5e0b42c417
karthikamurugesan123 <karthikamurugesan508@gmail.com>
2024-12-17 12:22:30 +0530

    changes made in code

--- Server.js
+++ Server.js
@@ -8,6 +8,15 @@ const UploadRoute = require("./routes/UploadRoute");
 
 const app = express();
 app.use(cors());
+
+const corsOptions = {
+  origin: '*',  // Allow only this domain
+  methods: ['GET', 'POST'],     // Allow only GET and POST requests
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify which headers can be sent
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.static("public"));
 
